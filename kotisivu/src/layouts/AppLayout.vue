828f348535594e55a8422ecaba9f7acb00738044
Schamppu <sami.pentti@gmail.com>
2020-07-08 22:28:05 +0300

    Initial commit

---
+++ kotisivu/src/layouts/AppLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Sami Pentin Online CV
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import EssentialLink from 'components/EssentialLink.vue'
+import {Component, Vue} from "vue-property-decorator";
+
+@Component({
+  components: {
+    EssentialLink
+  }
+})
+/**
+ * AppLayout luo verkkosivun perusrakenteen (header, navigation, yms.)
+ */
+export default class AppLayout extends Vue {
+  // Tyhjä toistaiseksi. Lisätään myöhemmin lisää
+}
+</script>
